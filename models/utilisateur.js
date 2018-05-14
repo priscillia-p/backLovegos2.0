56cf4beca0d95d3504ff1c874ea113a5db16a5a6
priscillia78 <priscillia.prehaud@outlook.com>
2018-05-14 17:04:09 +0200

    liason elastic

--- models/utilisateur.js
+++ models/utilisateur.js
@@ -8,57 +8,97 @@ var schema = mongoose.Schema;
 const UserSchema = mongoose.Schema({
     _id:{
         type: Number,
-        required: false
+        required: false,
+        es_indexed: true
+    },
+    dateNaissance:{
+        type:Date,
+        es_indexed:true
+    },
+    geoLoc:{
+        geo_point: {
+            type: String,
+            es_type: 'geo_point',
+            es_lat_lon: true,
+            es_indexed:true
+        },
+        lat:{
+            type: Number,
+            es_indexed:true
+        },
+        long:{
+            type: Number,
+            es_indexed:true
+        }
     },
     type:{
         type: String,
         required: true,
         trim: true,
+        es_indexed: false
     },
     login: {
         type: String,
         required: true,
         trim: true,
-        unique: true
+        unique: true,
+        es_indexed: false
     },
     mail:{type: String,
         required: true,
-        trim: true,},
-    nom:{type: String,
+        trim: true,
+        es_indexed: false
+    },
+    nom:{
+        type: String,
         required: true,
-        trim: true,},
+        trim: true,
+        es_indexed: true
+    },
     photo:{type: String,
         required: false,
-        trim: true,},
+        trim: true,
+        es_indexed: true
+    },
     prenom:{type: String,
         required: true,
-        trim: true,},
+        trim: true,
+        es_indexed: true
+    },
     statutPremium:{type: Boolean,
         required: false,
-        trim: true,},
+        trim: true,
+        es_indexed: false
+    },
     password: {
         type: String,
-        required: true
+        required: true,
+        es_indexed: false
     },
     motifs:[{
         type:String,
-        required:false
+        required:false,
+        es_indexed: true
     }],
     trancheAgeRecherche:[{
         type: Number,
-        required:false
+        required:false,
+        es_indexed: true
     }],
     genresRecherche:[{
         type:String,
-        required: false
+        required: false,
+        es_indexed: true
     }],
     genre:{
         type: String,
-        required: true
+        required: true,
+        es_indexed: true
     },
     presentation:{
         type: String,
-        required: false
+        required: false,
+        es_indexed: true
     }
 });
 
@@ -100,7 +140,10 @@ UserSchema.statics.authenticate = function(username, password, callback) {
 //UserSchema.plugin(mongoosastic);
 UserSchema.plugin(mongoosastic);
 var utilisateur = mongoose.model('utilisateurs', UserSchema);
-/*utilisateur.createMapping(function(err, mapping) {
+var stream = utilisateur.synchronize();
+var count = 0;
+
+utilisateur.createMapping(function(err, mapping) {
     if (err) {
       console.log('error creating mapping (you can safely ignore this)');
       console.log(err);
@@ -108,7 +151,18 @@ var utilisateur = mongoose.model('utilisateurs', UserSchema);
       console.log('mapping created!');
       console.log(mapping);
     }
-  });*/
+  });
+
+stream.on('data', function(err, doc){
+    count++;
+});
+stream.on('close', function(){
+    console.log('indexed ' + count + ' documents!');
+});
+stream.on('error', function(err){
+    console.log(err);
+});
+
 
   
 
